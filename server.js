f5f5fdbda58daf79c11c76d5c8c02fc0002ee80d
Alex <alex@ubuntu.(none)>
2011-12-16 23:58:50 +0000

    amended port

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ function start(route, handle) {
     route(handle, pathname, response, request);
   }
 
-  http.createServer(onRequest).listen(8888);
+  var port = process.env.PORT || 8888;
+  http.createServer(onRequest).listen(port);
   console.log("Server has started.");
 }
 
